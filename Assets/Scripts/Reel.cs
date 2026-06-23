68efdb24dd5b9b29de6a49e3056c0286662369df
AdityaGoku27 <254692047+AdityaGoku27@users.noreply.github.com>
2026-06-23 18:36:42 +0530

    Implemented infinite reel scroll

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -4,11 +4,15 @@ public class Reel : MonoBehaviour
 {
     [SerializeField] private float spinSpeed = 500f;
     private bool isSpinning;
+    [SerializeField] private float symbolSpacing = 150f;
+    private RectTransform[] symbols;
+    [SerializeField] private float bottomLimit = -700f;
 
 
     private void Start()
     {
         StartSpin();
+        symbols = GetComponentsInChildren<RectTransform>();
     }
     public void StartSpin()
     {
@@ -22,6 +26,41 @@ public class Reel : MonoBehaviour
             return;
         }
 
-        transform.Translate(Vector3.down * spinSpeed * Time.deltaTime);
+        foreach (RectTransform symbol in symbols)
+        {
+            if (symbol == transform)
+            {
+                continue;
+            }
+
+            symbol.Translate(Vector3.down * spinSpeed * Time.deltaTime);
+
+            if (symbol.localPosition.y < bottomLimit)
+            {
+                float highestY = GetHighestSymbolY();
+
+                symbol.localPosition = new Vector3(symbol.localPosition.x, highestY + symbolSpacing, symbol.localPosition.z);
+            }
+        }
+
+    }
+
+    private float GetHighestSymbolY()
+    {
+        float highestY = float.MinValue;
+
+        foreach (RectTransform symbol in symbols)
+        {
+            if (symbol == transform)
+            {
+                continue;
+            }
+
+            if (symbol.localPosition.y > highestY)
+            {
+                highestY = symbol.localPosition.y;
+            }
+        }
+        return highestY;
     }
 }
